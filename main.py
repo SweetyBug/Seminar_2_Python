c09c06d4fee82c4e7657ef756bde60b9c74d8736
SweetyBug <maxim_terekhin@bk.ru>
2022-07-12 23:46:09 +0300

    Finish code task_2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 def number(n):
-    n = n.replace('.', '')
-    n = n.replace('0', '')
-    return int(n)
+    if int(n) == 0:
+        return int(n)
+    else:
+        n = n.replace('.', '')
+        n = n.replace('0', '')
+        return int(n)
 
 def sum(data, c):
     if data != 0:
@@ -17,4 +20,14 @@ numb = input('Введите число: ')
 numb = number(numb)
 sum(numb, count)
 
-
+#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N
+def proizvedenie(numb, mas, i):
+    if len(mas) < n:
+        mas.append(i * mas[len(mas)-1])
+        proizvedenie(numb, mas, i+1)
+    else:
+        print(mas)
+n = int(input())
+j = 2
+arr = [1]
+proizvedenie(n, arr, j)
